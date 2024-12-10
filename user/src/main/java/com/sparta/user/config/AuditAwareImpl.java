2d3d402ef6ffbc713ab56b65154261d202a8bae1
MCshin00 <sinminc7@gmail.com>
2024-12-10 14:04:51 +0900

    refactor: AuditAwareImpl Component 어노테이션 추가

--- user/src/main/java/com/sparta/user/config/AuditAwareImpl.java
+++ user/src/main/java/com/sparta/user/config/AuditAwareImpl.java
@@ -1,12 +1,14 @@
-package com.sparta.user.audit;
+package com.sparta.user.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+@Component
 public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {

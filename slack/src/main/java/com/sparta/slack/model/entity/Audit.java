4752c8c7bf577bac25e0881c4909b701a1882f26
MCshin00 <sinminc7@gmail.com>
2024-12-11 01:05:03 +0900

    feat : Audit 기능 추가

---
+++ slack/src/main/java/com/sparta/slack/model/entity/Audit.java
@@ -0,0 +1,43 @@
+package com.sparta.slack.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Audit {
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+    private String deletedBy;
+
+    private LocalDateTime deletedAt;
+
+    @Column(name = "is_deleted")
+    private boolean deleted;
+
+    public void updateDeleted(String username) {
+        deletedBy = username;
+        deletedAt = LocalDateTime.now();
+        deleted = true;
+    }
+}

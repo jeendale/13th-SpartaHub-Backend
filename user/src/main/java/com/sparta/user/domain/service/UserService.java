87bda1e0c7f90a985bea1ef6fa5176adca5c7680
MCshin00 <sinminc7@gmail.com>
2024-12-10 20:44:47 +0900

    feat: 회원정보조회 기능 구현

--- user/src/main/java/com/sparta/user/domain/service/UserService.java
+++ user/src/main/java/com/sparta/user/domain/service/UserService.java
@@ -1,6 +1,7 @@
 package com.sparta.user.domain.service;
 
 import com.sparta.user.domain.dto.SignupRequestDto;
+import com.sparta.user.domain.dto.UserResponseDto;
 import com.sparta.user.domain.dto.UsernameResponseDto;
 import com.sparta.user.exception.UserExceptionMessage;
 import com.sparta.user.model.entity.User;
@@ -39,6 +40,20 @@ public class UserService {
                 .build();
     }
 
+    public UserResponseDto getUser(String requestUsername, UserRoleEnum requestRole, String username) {
+        // 요청 헤더의 role이 MASTER가 아닌 경우, 자신의 정보만 확인할 수 있음
+        if (requestRole != UserRoleEnum.MASTER) {
+            validateUsernameIsOwn(requestUsername, username);
+        }
+
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new IllegalArgumentException(UserExceptionMessage.USER_NOT_FOUND.getMessage()));
+
+        validateDeletedUser(user);
+
+        return UserResponseDto.from(user);
+    }
+
     private void validateUsername(String username) {
         Optional<User> checkUsername = userRepository.findById(username);
         if (checkUsername.isPresent()) {
@@ -60,9 +75,29 @@ public class UserService {
         }
     }
 
+    // 회원가입 시 Request Body의 role이 MASTER인지 검증하는 메서드
     private void validateRole(UserRoleEnum role) {
         if (role == UserRoleEnum.MASTER) {
             throw new IllegalArgumentException(UserExceptionMessage.NOT_ALLOWED_ROLE.getMessage());
+        }
+    }
+
+    // 요청 헤더의 role이 MASTER인지 검증하는 메서드
+    private void validateRequestRoleIsMaster(UserRoleEnum role) {
+        if (role != UserRoleEnum.MASTER) {
+            throw new IllegalArgumentException(UserExceptionMessage.NOT_ALLOWED_API.getMessage());
+        }
+    }
+
+    private void validateUsernameIsOwn(String requestUsername, String username) {
+        if (!requestUsername.equals(username)) {
+            throw new IllegalArgumentException(UserExceptionMessage.NOT_ALLOWED_API.getMessage());
+        }
+    }
+
+    private void validateDeletedUser(User user) {
+        if (user.isDeleted()) {
+            throw new IllegalArgumentException(UserExceptionMessage.DELETED_USER.getMessage());
         }
     }
 }

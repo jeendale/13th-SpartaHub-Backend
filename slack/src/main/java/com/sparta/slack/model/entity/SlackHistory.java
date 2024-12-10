7ecbcedec4a9937a4dc7f681b1aed5697791f6e6
MCshin00 <sinminc7@gmail.com>
2024-12-11 01:37:21 +0900

    feat : slack Entity 및 Repository 추가

---
+++ slack/src/main/java/com/sparta/slack/model/entity/SlackHistory.java
@@ -0,0 +1,33 @@
+package com.sparta.slack.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_slack_history")
+public class SlackHistory extends Audit{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID slackHistoryId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Lob
+    @Column(nullable = false)
+    private String message;
+}

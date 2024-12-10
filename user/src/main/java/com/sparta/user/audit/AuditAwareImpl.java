a6f494bce0ddf050caa1cc37f7c409267f7ac4aa
MCshin00 <sinminc7@gmail.com>
2024-12-10 12:46:37 +0900

    feat: User Entity 및 Auditing 구현

---
+++ user/src/main/java/com/sparta/user/audit/AuditAwareImpl.java
@@ -0,0 +1,27 @@
+package com.sparta.user.audit;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+
+        if (requestAttributes instanceof ServletRequestAttributes servletRequestAttributes) {
+            HttpServletRequest request = servletRequestAttributes.getRequest();
+            String usernameHeader = request.getHeader("X-Username");
+
+            if (usernameHeader != null) {
+                return Optional.of(usernameHeader);
+            }
+        }
+
+        // 헤더가 없거나 요청이 잘못된 경우
+        return Optional.empty();
+    }
+}

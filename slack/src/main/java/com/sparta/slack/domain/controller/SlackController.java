3185468fa752ce04952eeb0a7bd2da799d5bb7df
MCshin00 <sinminc7@gmail.com>
2024-12-11 12:16:33 +0900

    feat: 슬랙 메시지 기록 조회 기능 구현

--- slack/src/main/java/com/sparta/slack/domain/controller/SlackController.java
+++ slack/src/main/java/com/sparta/slack/domain/controller/SlackController.java
@@ -2,10 +2,14 @@ package com.sparta.slack.domain.controller;
 
 import com.sparta.slack.domain.dto.request.SlackRequestDto;
 import com.sparta.slack.domain.dto.response.SlackHistoryIdResponseDto;
+import com.sparta.slack.domain.dto.response.SlackHistoryResponseDto;
 import com.sparta.slack.domain.service.SlackService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -27,5 +31,15 @@ public class SlackController {
         SlackHistoryIdResponseDto responseDto = slackService.createSlackMessage(requestUsername, requestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/{slackHistoryId}")
+    public ResponseEntity<SlackHistoryResponseDto> getSlackHistory(
+            @PathVariable UUID slackHistoryId,
+            @RequestHeader("X-User-Role") String requestRole) {
+
+        SlackHistoryResponseDto responseDto = slackService.getSlackHistory(slackHistoryId, requestRole);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 }
--- slack/src/main/java/com/sparta/slack/domain/service/SlackService.java
+++ slack/src/main/java/com/sparta/slack/domain/service/SlackService.java
@@ -2,16 +2,20 @@ package com.sparta.slack.domain.service;
 
 import com.sparta.slack.domain.dto.request.SlackRequestDto;
 import com.sparta.slack.domain.dto.response.SlackHistoryIdResponseDto;
+import com.sparta.slack.domain.dto.response.SlackHistoryResponseDto;
+import com.sparta.slack.exception.SlackExceptionMessage;
 import com.sparta.slack.infrastructure.dto.SlackClientRequestDto;
 import com.sparta.slack.model.entity.SlackHistory;
 import com.sparta.slack.model.repository.SlackRepository;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SlackService {
 
     private final SlackRepository slackRepository;
@@ -36,5 +40,27 @@ public class SlackService {
         return SlackHistoryIdResponseDto.builder()
                 .slackHistoryId(slackHistory.getSlackHistoryId())
                 .build();
+    }
+
+    public SlackHistoryResponseDto getSlackHistory(UUID slackHistoryId, String requestRole) {
+        validateRequestRole(requestRole);
+
+        SlackHistory slackHistory = slackRepository.findById(slackHistoryId)
+                .orElseThrow(
+                        () -> new IllegalArgumentException(SlackExceptionMessage.SLACK_HISTORY_NOT_FOUND.getMessage()));
+
+        return SlackHistoryResponseDto.builder()
+                .slackHistoryId(slackHistory.getSlackHistoryId())
+                .username(slackHistory.getUsername())
+                .recivedSlackId(slackHistory.getRecievedSlackId())
+                .message(slackHistory.getMessage())
+                .sentAt(slackHistory.getSentAt())
+                .build();
+    }
+
+    private void validateRequestRole(String requestRole) {
+        if (!requestRole.equals("MASTER")) {
+            throw new IllegalArgumentException(SlackExceptionMessage.NOT_ALLOWED_API.getMessage());
+        }
     }
 }

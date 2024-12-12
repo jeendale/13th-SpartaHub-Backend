fa6922cadb93d24a349ffb75c391f995a7bf4323
MCshin00 <sinminc7@gmail.com>
2024-12-13 00:37:25 +0900

    feat: Company Entity 및 Repository 생성

---
+++ company/src/main/java/com/sparta/company/model/entity/Company.java
@@ -0,0 +1,37 @@
+package com.sparta.company.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_company")
+public class Company extends Audit{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID companyId;
+
+    @Column(nullable = false)
+    private UUID hubId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String companyName;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String companyAddress;
+}

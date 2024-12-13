b3009bb4dfcaf0f1e5236d27ec75661d983b549d
jeendale <jeekoo7203@gmail.com>
2024-12-13 23:15:06 +0900

    fix : role권한확인 표시

--- Hub/src/main/java/com/sparta/Hub/domain/service/HubRouteService.java
+++ Hub/src/main/java/com/sparta/Hub/domain/service/HubRouteService.java
@@ -43,7 +43,7 @@ public class HubRouteService {
       String requestUsername,
       String requestRole
   ) {
-    //validateRole(requestRole);
+    validateRole(requestRole);
     validateEquealHub(createHubRouteReq);
 
     Hub startHub = validateHub(createHubRouteReq.getStratHubId());
@@ -77,6 +77,7 @@ public class HubRouteService {
         .build();
 
   }
+
   private void validateRole(String requestRole) {
     if(!requestRole.equals("MASTER")){
       throw new IllegalArgumentException(HubExceptionMessage.NOT_ALLOWED_API.getMessage());

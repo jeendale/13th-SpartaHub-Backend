d89c2c6d6bb548fb2bb430fb0b1933b7c76f11e3
MCshin00 <sinminc7@gmail.com>
2024-12-15 23:31:13 +0900

    refactor: Valid 어노테이션에서 발생하는 에러 핸들러 추가

--- user/src/main/java/com/sparta/user/exception/GlobalExceptionHandler.java
+++ user/src/main/java/com/sparta/user/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sparta.user.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<RestApiException> illegalArgumentException(final IllegalArgumentException ex) {
 
         log.warn("IllegalArgumentException 발생: {}", ex.getMessage());
+
+        RestApiException restApiException = RestApiException.builder()
+                .errorMessage(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RestApiException> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        log.warn("handleValidationExceptions 발생: {}", ex.getMessage());
 
         RestApiException restApiException = RestApiException.builder()
                 .errorMessage(ex.getMessage())
